1369a2f40168e5e5c38b804c074d1a9ff49ddbd7
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-09-08 10:28:48 +0900

    Remove strictmiddleware/iris

--- strictmiddleware/iris/main.go
+++
@@ -1,9 +0,0 @@
-package iris
-
-import (
-	"github.com/kataras/iris/v12"
-)
-
-type StrictIrisHandlerFunc func(ctx iris.Context, request interface{}) (response interface{}, err error)
-
-type StrictIrisMiddlewareFunc func(f StrictIrisHandlerFunc, operationID string) StrictIrisHandlerFunc
