4398d48b46b2f285e020fe7fe0a88c10de07092f
Ashutosh Kumar <sonasingh46@gmail.com>
2023-12-21 20:36:03 +0530

    improve nullable type
    Signed-off-by: Ashutosh Kumar <sonasingh46@gmail.com>

--- types/nullable.go
+++ types/nullable.go
@@ -2,25 +2,35 @@ package types
 
 import "encoding/json"
 
+// Nullable type which can distinguish between an explicit `null` vs not provided
+// in JSON when un-marshaled to go type.
 type Nullable[T any] struct {
-	Value T
-	Set   bool
-	Null  bool
+	// Value is the actual value of the field.
+	Value *T
+	// Defined indicates that the field was provided in JSON if it is true.
+	// If a field is not provided in JSON, then `Defined` is false and `Value`
+	// contains the `zero-value` of the field type e.g "" for string,
+	// 0 for int, nil for pointer etc
+	Defined bool
 }
 
+// UnmarshalJSON implements the Unmarshaler interface.
 func (t *Nullable[T]) UnmarshalJSON(data []byte) error {
-	t.Set = true
+	t.Defined = true
 	return json.Unmarshal(data, &t.Value)
 }
 
+// MarshalJSON implements the  Marshaler interface.
 func (t Nullable[T]) MarshalJSON() ([]byte, error) {
 	return json.Marshal(t.Value)
 }
 
-func (t *Nullable[T]) IsNullDefined() bool {
-	return t.Set && t.Value == nil
+// IsNull returns true if the value is explicitly provided `null` in json
+func (t *Nullable[T]) IsNull() bool {
+	return t.IsDefined() && t.Value == nil
 }
 
-func (t *Nullable[T]) HasValue() bool {
-	return t.Set && t.Value != nil
+// IsDefined returns true if the value is explicitly provided in json
+func (t *Nullable[T]) IsDefined() bool {
+	return t.Defined
 }
--- types/nullable_test.go
+++ types/nullable_test.go
@@ -1,1 +1,242 @@
 package types
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type SimpleString struct {
+	Name Nullable[string] `json:"name"`
+}
+
+func TestSimpleString_IsDefined(t *testing.T) {
+	type testCase struct {
+		name        string
+		jsonInput   []byte
+		wantNull    bool
+		wantDefined bool
+	}
+	tests := []testCase{
+		{
+			name:      "simple object: set name to some non null value",
+			jsonInput: []byte(`{"name":"yolo"}`),
+			// since name field is present in JSON and is NOT null, want null to be false
+			wantNull: false,
+			// since name field is present in JSON, want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: set name to empty string value",
+			jsonInput: []byte(`{"name":""}`),
+			// since name field is present in JSON and is NOT null, want null to be false
+			wantNull: false,
+			// since name field is present in JSON, want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: set name to null value",
+			jsonInput: []byte(`{"name":null}`),
+			// since name field is present in JSON and is  null, want null to be true
+			wantNull: true,
+			// since name field is present in JSON, want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: do not provide name in json data",
+			jsonInput: []byte(`{}`),
+			// since name field is NOT present in JSON, want null to be false
+			wantNull: false,
+			// since name field is present in JSON, want defined to be false
+			wantDefined: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t1 *testing.T) {
+			var obj SimpleString
+			err := json.Unmarshal(tt.jsonInput, &obj)
+			assert.NoError(t, err)
+			assert.Equalf(t, tt.wantNull, obj.Name.IsNull(), "IsNull()")
+			assert.Equalf(t, tt.wantDefined, obj.Name.IsDefined(), "IsDefined()")
+		})
+	}
+}
+
+type SimpleStringPointer struct {
+	Name Nullable[*string] `json:"name"`
+}
+
+func TestSimpleStringPointer_IsDefined(t *testing.T) {
+	type testCase struct {
+		name        string
+		jsonInput   []byte
+		wantNull    bool
+		wantDefined bool
+	}
+	tests := []testCase{
+		{
+			name:      "simple object: set name to some non null value",
+			jsonInput: []byte(`{"name":"yolo"}`),
+			// since name field is present in JSON and is NOT null, want null to be false
+			wantNull: false,
+			// since name field is present in JSON, want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: set name to empty string value",
+			jsonInput: []byte(`{"name":""}`),
+			// since name field is present in JSON and is NOT null, want null to be false
+			wantNull: false,
+			// since name field is present in JSON, want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: set name to null value",
+			jsonInput: []byte(`{"name":null}`),
+			// since name field is present in JSON and is  null, want null to be true
+			wantNull: true,
+			// since name field is present in JSON, want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: do not provide name in json data",
+			jsonInput: []byte(`{}`),
+			// since name field is NOT present in JSON, want null to be false
+			wantNull: false,
+			// since name field is present in JSON, want defined to be false
+			wantDefined: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t1 *testing.T) {
+			var obj SimpleStringPointer
+			err := json.Unmarshal(tt.jsonInput, &obj)
+			assert.NoError(t, err)
+			assert.Equalf(t, tt.wantNull, obj.Name.IsNull(), "IsNull()")
+			assert.Equalf(t, tt.wantDefined, obj.Name.IsDefined(), "IsDefined()")
+		})
+	}
+}
+
+type SimpleInt struct {
+	ReplicaCount Nullable[int] `json:"replicaCount"`
+}
+
+func TestSimpleInt_IsDefined(t *testing.T) {
+	type testCase struct {
+		name        string
+		jsonInput   []byte
+		wantNull    bool
+		wantDefined bool
+	}
+	tests := []testCase{
+		{
+			name:      "simple object: set name to some non null value",
+			jsonInput: []byte(`{"replicaCount":1}`),
+			// since replicaCount field is present in JSON but is NOT null want null to be false
+			wantNull: false,
+			// since name field is present in JSON want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: set name to empty value",
+			jsonInput: []byte(`{"replicaCount":0}`),
+			// since replicaCount field is present in JSON but is NOT null want null to be false
+			wantNull: false,
+			// since name field is present in JSON want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: set name to null value",
+			jsonInput: []byte(`{"replicaCount":null}`),
+			// since replicaCount field is present in JSON and is null, want null to be true
+			wantNull: true,
+			// since name field is present in JSON want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: do not provide name in json data",
+			jsonInput: []byte(`{}`),
+			// since name field is NOT present in JSON, want null to be false
+			wantNull: false,
+			// since name field is NOT present in JSON want defined to be false
+			wantDefined: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t1 *testing.T) {
+			var obj SimpleInt
+			err := json.Unmarshal(tt.jsonInput, &obj)
+			assert.NoError(t, err)
+			assert.Equalf(t, tt.wantNull, obj.ReplicaCount.IsNull(), "IsNull()")
+			assert.Equalf(t, tt.wantDefined, obj.ReplicaCount.IsDefined(), "IsDefined()")
+		})
+	}
+}
+
+type SimpleIntPointer struct {
+	ReplicaCount Nullable[*int] `json:"replicaCount"`
+}
+
+func TestSimpleIntPointer_IsDefined(t *testing.T) {
+	type testCase struct {
+		name        string
+		jsonInput   []byte
+		wantNull    bool
+		wantDefined bool
+	}
+	tests := []testCase{
+		{
+			name:      "simple object: set name to some non null value",
+			jsonInput: []byte(`{"replicaCount":1}`),
+			// since replicaCount field is present in JSON but is NOT null, want null false
+			wantNull: false,
+			// since replicaCount field is present in JSON want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: set name to empty value",
+			jsonInput: []byte(`{"replicaCount":0}`),
+			// since replicaCount field is present in JSON but is NOT null, want null false
+			wantNull: false,
+			// since replicaCount field is present in JSON want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: set name to null value",
+			jsonInput: []byte(`{"replicaCount":null}`),
+			// since replicaCount field is present in JSON and is null, want null true
+			wantNull: true,
+			// since replicaCount field is present in JSON want defined to be true
+			wantDefined: true,
+		},
+
+		{
+			name:      "simple object: do not provide name in json data",
+			jsonInput: []byte(`{}`),
+			// since replicaCount field is NOT present in JSON, want null false
+			wantNull: false,
+			// since replicaCount field is NOT present in JSON want defined to be false
+			wantDefined: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t1 *testing.T) {
+			var obj SimpleIntPointer
+			err := json.Unmarshal(tt.jsonInput, &obj)
+			assert.NoError(t, err)
+			assert.Equalf(t, tt.wantNull, obj.ReplicaCount.IsNull(), "IsNull()")
+		})
+	}
+}
