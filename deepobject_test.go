87a495bebc43833aa10c86dac47870e790bc2968
Daniel Castro <danicc097@gmail.com>
2024-03-09 09:43:40 +0100

    test update

--- deepobject_test.go
+++ deepobject_test.go
@@ -10,27 +10,38 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type InnerArrayObject struct {
+	Names []string `json:"names"`
+}
+
 type InnerObject struct {
 	Name string
 	ID   int
 }
+type InnerObject2 struct {
+	Foo string
+	Is  bool
+}
 
 // These are all possible field types, mandatory and optional.
 type AllFields struct {
-	I   int             `json:"i"`
-	Oi  *int            `json:"oi,omitempty"`
-	F   float32         `json:"f"`
-	Of  *float32        `json:"of,omitempty"`
-	B   bool            `json:"b"`
-	Ob  *bool           `json:"ob,omitempty"`
-	As  []string        `json:"as"`
-	Oas *[]string       `json:"oas,omitempty"`
-	O   InnerObject     `json:"o"`
-	Oo  *InnerObject    `json:"oo,omitempty"`
-	D   MockBinder      `json:"d"`
-	Od  *MockBinder     `json:"od,omitempty"`
-	M   map[string]int  `json:"m"`
-	Om  *map[string]int `json:"om,omitempty"`
+	I    int              `json:"i"`
+	Oi   *int             `json:"oi,omitempty"`
+	Ab   *[]bool          `json:"ab,omitempty"`
+	F    float32          `json:"f"`
+	Of   *float32         `json:"of,omitempty"`
+	B    bool             `json:"b"`
+	Ob   *bool            `json:"ob,omitempty"`
+	As   []string         `json:"as"`
+	Oas  *[]string        `json:"oas,omitempty"`
+	O    InnerObject      `json:"o"`
+	Ao   []InnerObject2   `json:"ao"`
+	Onas InnerArrayObject `json:"onas"`
+	Oo   *InnerObject     `json:"oo,omitempty"`
+	D    MockBinder       `json:"d"`
+	Od   *MockBinder      `json:"od,omitempty"`
+	M    map[string]int   `json:"m"`
+	Om   *map[string]int  `json:"om,omitempty"`
 }
 
 func TestDeepObject(t *testing.T) {
@@ -54,11 +65,19 @@ func TestDeepObject(t *testing.T) {
 		Of:  &of,
 		B:   true,
 		Ob:  &ob,
+		Ab:  &[]bool{true},
 		As:  []string{"hello", "world"},
 		Oas: &oas,
 		O: InnerObject{
 			Name: "Joe Schmoe",
 			ID:   456,
+		},
+		Ao: []InnerObject2{
+			{Foo: "bar", Is: true},
+			{Foo: "baz", Is: false},
+		},
+		Onas: InnerArrayObject{
+			Names: []string{"Bill", "Frank"},
 		},
 		Oo: &oo,
 		D:  d,
@@ -69,7 +88,7 @@ func TestDeepObject(t *testing.T) {
 
 	marshaled, err := MarshalDeepObject(srcObj, "p")
 	require.NoError(t, err)
-	t.Log(marshaled)
+	require.EqualValues(t, "p[ab][0]=true&p[ao][0][Foo]=bar&p[ao][0][Is]=true&p[ao][1][Foo]=baz&p[ao][1][Is]=false&p[as][0]=hello&p[as][1]=world&p[b]=true&p[d]=2020-02-01&p[f]=4.2&p[i]=12&p[m][additional]=1&p[o][ID]=456&p[o][Name]=Joe Schmoe&p[oas][0]=foo&p[oas][1]=bar&p[ob]=true&p[od]=2020-02-01&p[of]=3.7&p[oi]=5&p[om][additional]=1&p[onas][names][0]=Bill&p[onas][names][1]=Frank&p[oo][ID]=123&p[oo][Name]=Marcin Romaszewicz", marshaled)
 
 	params := make(url.Values)
 	marshaledParts := strings.Split(marshaled, "&")
